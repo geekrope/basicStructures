8521a4fc35794209abd104d2012be01815828bdc
geekrope <geekrope@gmail.com>
2024-06-10 12:14:45 +0400

    Graph algorithms signature

---
+++ basicStructures/GraphAlgorithms.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace basicStructures
+{
+    internal class GraphAlgorithms
+    {
+        public IEnumerable<int> DFS(int[][] graph)
+        {
+            throw new NotImplementedException();
+        }
+        public int[] BFS(int[][] graph)
+        {
+            throw new NotImplementedException();
+        }
+        public bool IsSeparable(int[][] graph, int size)
+        {
+            throw new NotImplementedException();
+        }
+        public bool FindComponents(int[][] graph, int size)
+        {
+            throw new NotImplementedException();
+        }
+        public int[] Dijkstra(int[][] graph, int size, int start)
+        {
+            throw new NotImplementedException();
+        }
+        public int[,] Floyd(int[][] graph, int size)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
